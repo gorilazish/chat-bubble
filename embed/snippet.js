3be38a2d82b41ece5b57d5bb1a3ca5fa5d67201f
Martynas J <lyrinisobjektas@gmail.com>
2018-02-01 15:14:20 +0000

    Simplify snippet

--- embed/snippet.js
+++ embed/snippet.js
@@ -5,14 +5,10 @@
     userId: 'martynas'
   }
  
-  var proto = document.location.protocol,
-    host = "widget.belloforwork.com",
-    url = proto + "//" + host
- 
   var s = document.createElement("script")
-  s.type = "text/javascript"
-  s.async = true
-  s.src = url + "/widget-loader.js"
+    s.type = "text/javascript"
+    s.async = true
+    s.src = "//widget.belloforwork.com/widget-loader.js"
  
   var x = document.getElementsByTagName("script")[0]
   x.parentNode.insertBefore(s, x)
