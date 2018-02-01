f3ad1f0a6fe1fa094ba5a89a06d92ca73607a064
Martynas J <lyrinisobjektas@gmail.com>
2018-02-01 16:07:58 +0000

    Add css file to widget-loader

--- embed/widget-loader.js
+++ embed/widget-loader.js
@@ -1,23 +1,40 @@
 module.exports = function(compilation) {
-  const reg = /main\.(.+).js$/
   const url = '//widget.belloforwork.com'
 
+  const reg = /main\.(.+).js$/
+  const cssReg = /main\.(.+).css$/
+
   let mainFile
+  let mainCssFile
+  
   compilation.chunks.forEach(chunk => {
     chunk.files.forEach(filename => {
       if (reg.test(filename)) {
         mainFile = filename
+      } else if (cssReg.test(filename)) {
+        mainCssFile = filename
       }
     })
   })
 
-  if (!mainFile) {
+  if (!mainFile || !mainCssFile) {
     throw new Error('Failed to generate widget-loader. Could not find main bundle file')
   }
 
-  return createTemplate(`${url}/${mainFile}`)
+  return createTemplate(`${url}/${mainFile}`, `${url}/${mainCssFile}`)
 }
 
-function createTemplate(bundleSrcUrl) {
-  return `(function(){var d=document;var e=d.createElement('script');e.type='text/javascript';e.async=true;e.src = '${bundleSrcUrl}';var body=d.getElementsByTagName('body')[0];body.appendChild(e);})()`
+function createTemplate(bundleSrcUrl, bundleCssUrl) {
+  return `(function(){
+    var d = document;
+    var e = d.createElement('script');
+    e.type = 'text/javascript';
+    e.async = true;
+    e.src = '${bundleSrcUrl}';
+    var body = d.getElementsByTagName('body')[0];
+    var l = d.createElement('link')
+    l.href = '${bundleCssUrl}'
+    l.rel = 'stylesheet'
+    d.head.appendChild(l)
+    body.appendChild(e);})()`
 }
