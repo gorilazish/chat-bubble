784bc9af5b19de4ebc271fde74f7da5b3f3adeb0
Martynas J <lyrinisobjektas@gmail.com>
2018-02-01 11:59:54 +0000

    Generate widget loader with injected main bundle

--- embed/widget-loader.js
+++ embed/widget-loader.js
@@ -1,9 +1,23 @@
-(function() {
-  var scriptElement = document.createElement('script')
-  var revision = 'e1d235'
-  scriptElement.type = 'text/javascript'
-  scriptElement.async = true
-  scriptElement.src = '//bello-widget.firebaseapp.com/main.' + revision + '.js'
-  var body = document.getElementsByTagName('body')[0]
-  body.appendChild(scriptElement)
-})()
+module.exports = function(compilation) {
+  const reg = /main\.(.+).js$/
+  const url = '//widget.belloforwork.com'
+
+  let mainFile
+  compilation.chunks.forEach(chunk => {
+    chunk.files.forEach(filename => {
+      if (reg.test(filename)) {
+        mainFile = filename
+      }
+    })
+  })
+
+  if (!mainFile) {
+    throw new Error('Failed to generate widget-loader. Could not find main bundle file')
+  }
+
+  return createTemplate(`${url}/${mainFile}`)
+}
+
+function createTemplate(bundleSrcUrl) {
+  return `(function(){var d=document;var e=d.createElement('script');e.type='text/javascript';e.async=true;e.src = '${bundleSrcUrl}';var body=d.getElementsByTagName('body')[0];body.appendChild(e);})()`
+}
